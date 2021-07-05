69fa0568c66eb6ed369599ee9eb9372409a894cf
Steve Chan <steve@oscer.ai>
2021-07-06 00:40:30 +1000

    feat: add detailed blogs

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,5 @@ from . import views
 
 urlpatterns = [
     path('', views.allblogs, name="allblogs"),
+    path('<int:blog_id>/', views.detail, name='detail')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import Blog
 
@@ -8,3 +8,8 @@ from .models import Blog
 def allblogs(request):
     blogs = Blog.objects
     return render(request, 'blog/allblogs.html', {'blogs': blogs})
+
+
+def detail(request, blog_id):
+    detailBlog = get_object_or_404(Blog, pk=blog_id)
+    return render(request, 'blog/detail.html', {'blog': detailBlog})
